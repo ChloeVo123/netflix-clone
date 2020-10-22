0a34bf930666df4c94efd74d8524def129798312
HangVo123 <48342625+HangVo123@users.noreply.github.com>
2020-10-22 18:03:24 +1100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCq90A5_IZ-N2ysobjFJYGE2Rt4reWyrc8",
+    authDomain: "netflix-clone-f1c8e.firebaseapp.com",
+    databaseURL: "https://netflix-clone-f1c8e.firebaseio.com",
+    projectId: "netflix-clone-f1c8e",
+    storageBucket: "netflix-clone-f1c8e.appspot.com",
+    messagingSenderId: "947739557167",
+    appId: "1:947739557167:web:12e7044a87544ce08ab5da",
+    measurementId: "G-JMZXF1WLZX"
+  };
